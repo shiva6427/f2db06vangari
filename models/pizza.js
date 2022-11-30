639cfb4b774d4fd08b368f5edad0ce1a836b15fc
Shiva Vangari <49754508+shiva6427@users.noreply.github.com>
2022-11-30 17:46:13 -0600

    added bonus

--- models/pizza.js
+++ models/pizza.js
@@ -3,15 +3,18 @@ const pizzaSchema = mongoose.Schema({
  pizza_type: {
     type:String,
     required:["Pizza name is required."],
+    required:["Pizza name cannot be empty."]
  }, 
  size:  {
     type:String,
     required:["Pizza size is required."],
+    required:["Pizza size cannot be empty."]
  },
  cost:{
     type: Number,
     min:[15,"Cost Should be minimum of 15$ "],
-    max:[100,"Cost Cannot be greater than 50$ "]
+    max:[50,"Cost Cannot be greater than 50$ "],
+    required:["Pizza cost cannot be empty."]
 }
 })
 
